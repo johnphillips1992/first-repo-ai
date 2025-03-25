4c4ae7b6abf1b1095e00ceab9fbdaeed72992b3e
John Phillips <john.phillips1992@gmail.com>
2025-03-24 22:38:24 -0700

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreateMixtapePage from './pages/CreateMixtapePage';
-import MixtapePlayerPage from './pages/MixtapePlayerPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ProfilePage from './pages/ProfilePage';
-import DiscoverPage from './pages/DiscoverPage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
 import { useAuth } from './contexts/AuthContext';
 import './App.css';
@@ -22,14 +15,9 @@ const App: React.FC = () => {
   
   return (
     <div className="app">
-      <Navbar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/discover" element={<DiscoverPage />} />
-          <Route path="/mixtape/:id" element={<MixtapePlayerPage />} />
           <Route 
             path="/create" 
             element={
@@ -38,17 +26,8 @@ const App: React.FC = () => {
               </ProtectedRoute>
             } 
           />
-          <Route 
-            path="/profile" 
-            element={
-              <ProtectedRoute>
-                <ProfilePage />
-              </ProtectedRoute>
-            } 
-          />
         </Routes>
       </main>
-      <Footer />
     </div>
   );
 };
